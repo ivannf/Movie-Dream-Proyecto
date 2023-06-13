78cbec9878a748ef78f20d5cd16090990cfa9e28
ivannf <ivannf2212@gmail.com>
2023-06-13 19:32:21 +0200

    MovieDream

--- src-frontend/src/app/modules/detalles-pelicula/detalles-pelicula.component.ts
+++ src-frontend/src/app/modules/detalles-pelicula/detalles-pelicula.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Pelicula } from './pelicula.model';
 import { MovieService } from 'src/app/services/movie.service';
-
-
+import { DataService } from 'src/app/services/data.service';
 @Component({
   selector: 'app-detalles-pelicula',
   templateUrl: './detalles-pelicula.component.html',
@@ -11,11 +10,17 @@ import { MovieService } from 'src/app/services/movie.service';
 })
 export class DetallesPeliculaComponent implements OnInit {
   pelicula: any;
+  correo: string;
 
   constructor(
     private router: Router,
-    private movieService: MovieService
-  ) { }
+    private movieService: MovieService,
+    private route: ActivatedRoute,
+    private dataService: DataService
+  ) { 
+    this.correo = this.dataService.getCorreo();
+    console.log(this.correo);
+  }
 
   ngOnInit() {
     this.getDetallePelicula();
@@ -30,5 +35,11 @@ export class DetallesPeliculaComponent implements OnInit {
         console.log('Error al obtener la pelicula', error);
       }
     );
+  }
+
+  reservar(): void {
+    const url = this.route.snapshot.url;
+    const lastSegment = url[url.length - 1].path;
+    this.router.navigate([`/reservaPelicula/${lastSegment}`]);
   }
 }
